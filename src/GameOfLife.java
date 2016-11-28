a476a957f391885db6996b0c9c3e6bbc51c135f8
sergey.sharkov <ssharkov29@ukr.net>
2016-11-28 14:40:33 +0200

    test

--- src/GameOfLife.java
+++ src/GameOfLife.java
@@ -26,14 +26,22 @@ public class GameOfLife {
 //        }
 
         //--------------------------------------------------------------^^^ array was populated version 2
+
         char world[][] = {
                 {'*', '*', '*', '*', '*'},
-                {'*', 'X', '*', '*', '*'},
-                {'*', 'X', 'X', '*', '*'},
-                {'*', '*', '*', '*', '*'},
+                {'*', '*', 'X', '*', '*'},
+                {'*', '*', '*', 'X', '*'},
+                {'*', 'X', 'X', 'X', '*'},
                 {'*', '*', '*', '*', '*'}
         };
-        char newworld[][] = new char[5][5];
+//        char newworld[][] = new char[5][5];
+        char newworld[][] = {
+                {'*', '*', '*', '*', '*'},
+                {'*', '*', 'X', '*', '*'},
+                {'*', '*', '*', 'X', '*'},
+                {'*', 'X', 'X', 'X', '*'},
+                {'*', '*', '*', '*', '*'}
+        };
 
         int c = 0, i, j;
         char x = 'X';
@@ -44,42 +52,43 @@ public class GameOfLife {
                 System.out.print(world[i][j] + " ");
             System.out.println();
         }
+        System.out.println();
 
-        for (i = 0; i < 5; i++) {
-            for (j = 0; j < 5; j++) {
-                c = 0;
+        for (int r=0;r<5;r++) {
 
-                for (int row = i - 1; row <= i + 1; row++) {
-                    if (row < 0 || row > 4) continue;
+            for (i = 0; i < 5; i++) {
+                for (j = 0; j < 5; j++) {
+                    c = 0;
 
-                    for (int cell = j - 1; cell <= j + 1; cell++) {
+                    for (int row = i - 1; row <= i + 1; row++) {
+                        if (row < 0 || row > 4) continue;
 
-                        if (cell < 0 || cell > 4) continue;
+                        for (int col = j - 1; col <= j + 1; col++) {
 
-                        if (row == i && cell == j) continue;
-                        if (world[row][cell] == x) c++;
+                            if (col < 0 || col > 4) continue;
+                            if (row == i && col == j) continue;
+                            if (world[row][col] == x) c++;
+                        }
                     }
+                    if (c < 2 || c > 3) {
+                        newworld[i][j] = o;
+                    }
+                    if (c == 2) {
+                        newworld[i][j] = world[i][j];
+                    }
+                    if (c == 3) {
+                        newworld[i][j] = x;
+                    }
+                    System.out.print(newworld[i][j] + " ");
                 }
-                System.out.print(c + " ");
+                System.out.println();
+
             }
+            world = newworld;
             System.out.println();
         }
 
 
-//        for (i = 1; i < 2; i++)
-//            for (j = 2; j < 3; j++) {
-//                if (world[i - 1][j - 1] == x) c = c + 1;
-//                if (world[i - 1][j] == x) c = c + 1;
-//                if (world[i - 1][j + 1] == x) c = c + 1;
-//                if (world[i][j - 1] == x) c = c + 1;
-//                if (world[i][j + 1] == x) c = c + 1;
-//                if (world[i + 1][j - 1] == x) c = c + 1;
-//                if (world[i + 1][j] == x) c = c + 1;
-//                if (world[i + 1][j + 1] == x) c = c + 1;
-//                if (c==3) world[i][j] =x;
-//            }
-//        System.out.println(c);
-
-
     }
+
 }
