1d1d63936709caffc3677f848b6522cb681e6df0
sergey.sharkov <ssharkov29@ukr.net>
2017-02-02 19:19:15 +0200

    test

--- src/Calendar/CalendarDemo.java
+++ src/Calendar/CalendarDemo.java
@@ -9,10 +9,26 @@ public class CalendarDemo {
     public static void main(String[] args) {
         InMemoryDemo e = new InMemoryDemo();
 
-
         LocalDateTime ldt = LocalDateTime.parse("2017-02-01T10:00:00");
-        System.out.println("Date: " + ldt);
+        LocalDateTime ldt1 = LocalDateTime.parse("2017-02-02T10:00:00");
+        LocalDateTime ldt2 = LocalDateTime.parse("2017-02-03T10:00:00");
 
-//        e.addEvent(ldt, "test event","Room 100");
+        Event event = new Event(ldt, "test event", "Room 100");
+        Event event1 = new Event(ldt1, "test event one ", "Room 101");
+        Event event2 = new Event(ldt2, "test event two", "Room 102");
+
+        e.addEvent(event);
+        e.addEvent(event1);
+        e.addEvent(event2);
+
+        System.out.println(e.getEvent(ldt,ldt2));
+        System.out.println();
+
+
+//        System.out.println(e);
+
+
+
+
     }
 }
