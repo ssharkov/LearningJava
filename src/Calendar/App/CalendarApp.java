18ebbc59f31ecb7387aab2711fcaf96f08d7621b
sergey.sharkov <ssharkov29@ukr.net>
2017-02-21 17:49:45 +0200

    test

---
+++ src/Calendar/App/CalendarApp.java
@@ -0,0 +1,19 @@
+package Calendar.App;/**
+ * Created by ssharkov on 14.02.2017.
+ */
+
+import Calendar.Calendar;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class CalendarApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
---
+++ src/Learning/Desktop/Address/View/PersonOverviewController.java
@@ -0,0 +1,123 @@
+package Learning.Desktop.Address.View;
+
+import Learning.Desktop.Address.MainApp;
+import Learning.Desktop.Address.Model.Person;
+import Learning.Desktop.Address.Util.DateUtil;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+/**
+ * Created by ssharkov on 20.02.2017.
+ */
+public class PersonOverviewController {
+    @FXML
+    private TableView<Person> personTable;
+    @FXML
+    private TableColumn<Person, String> firstNameColumn;
+    @FXML
+    private TableColumn<Person, String> lastNameColumn;
+
+    @FXML
+    private Label firstNameLabel;
+    @FXML
+    private Label lastNameLabel;
+    @FXML
+    private Label streetLabel;
+    @FXML
+    private Label postalCodeLabel;
+    @FXML
+    private Label cityLabel;
+    @FXML
+    private Label birthdayLabel;
+
+    // Ссылка на главное приложение.
+    private MainApp mainApp;
+
+    /**
+     * Конструктор.
+     * Конструктор вызывается раньше метода initialize().
+     */
+    public PersonOverviewController() {
+    }
+
+    /**
+     * Инициализация класса-контроллера. Этот метод вызывается автоматически
+     * после того, как fxml-файл будет загружен.
+     */
+    @FXML
+    private void initialize() {
+        // Инициализация таблицы адресатов с двумя столбцами.
+        firstNameColumn.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
+        lastNameColumn.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
+        // Очистка дополнительной информации об адресате.
+        showPersonDetails(null);
+
+        // Слушаем изменения выбора, и при изменении отображаем
+        // дополнительную информацию об адресате.
+        personTable.getSelectionModel().selectedItemProperty().addListener(
+                (observable, oldValue, newValue) -> showPersonDetails(newValue));
+    }
+
+    /**
+     * Вызывается главным приложением, которое даёт на себя ссылку.
+     *
+     * @param mainApp
+     */
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+
+        // Добавление в таблицу данных из наблюдаемого списка
+        personTable.setItems(mainApp.getPersonData());
+    }
+    /**
+     * Заполняет все текстовые поля, отображая подробности об адресате.
+     * Если указанный адресат = null, то все текстовые поля очищаются.
+     *
+     * @param person — адресат типа Person или null
+     */
+    private void showPersonDetails(Person person) {
+        if (person != null) {
+            // Заполняем метки информацией из объекта person.
+            firstNameLabel.setText(person.getFirstName());
+            lastNameLabel.setText(person.getLastName());
+            streetLabel.setText(person.getStreet());
+            postalCodeLabel.setText(Integer.toString(person.getPostalCode()));
+            cityLabel.setText(person.getCity());
+
+            // TODO: Нам нужен способ для перевода дня рождения в тип String!
+            birthdayLabel.setText(DateUtil.format(person.getBirthday()));
+            // birthdayLabel.setText(...);
+        } else {
+            // Если Person = null, то убираем весь текст.
+            firstNameLabel.setText("");
+            lastNameLabel.setText("");
+            streetLabel.setText("");
+            postalCodeLabel.setText("");
+            cityLabel.setText("");
+            birthdayLabel.setText("");
+        }
+    }
+
+    /**
+     * Вызывается, когда пользователь кликает по кнопке удаления.
+     */
+    @FXML
+    private void handleDeletePerson() {
+        int selectedIndex = personTable.getSelectionModel().getSelectedIndex();
+        if (selectedIndex >= 0) {
+            personTable.getItems().remove(selectedIndex);
+        } else {
+            // Ничего не выбрано.
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.initOwner(mainApp.getPrimaryStage());
+            alert.setTitle("No Selection");
+            alert.setHeaderText("No Person Selected");
+            alert.setContentText("Please select a person in the table.");
+
+            alert.showAndWait();
+        }
+    }
+}
